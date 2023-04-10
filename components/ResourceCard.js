c54b94825fb4c85d9707562e24f8b1602604e395
kvin-stphns <kvin.stphns@gmail.com>
2023-04-10 03:13:25 -0400

    Update ResourceCard.js

--- components/ResourceCard.js
+++ components/ResourceCard.js
@@ -1,14 +1,23 @@
+// components/ResourceCard.js
 import React from 'react';
+import ResourceTag from './ResourceTag'; // Add this import
 import styles from './ResourceCard.module.css';
 
 const ResourceCard = ({ resource }) => {
-  const { title, description, url, imageUrl } = resource;
+  const { title, description, url, imageUrl, tags } = resource;
 
   return (
     <div className={styles.resourceCard}>
       <img className={styles.image} src={imageUrl} alt={title} />
       <h3 className={styles.title}>{title}</h3>
       <p className={styles.description}>{description}</p>
+      {tags && (
+        <div className={styles.tags}>
+          {tags.map((tag, index) => (
+            <ResourceTag key={index} tag={tag} />
+          ))}
+        </div>
+      )}
       <a className={styles.link} href={url} target="_blank" rel="noreferrer">
         Learn More
       </a>
