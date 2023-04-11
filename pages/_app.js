4df513e5546a85535aec5a69d685997e07785d2c
kvin-stphns <kvin.stphns@gmail.com>
2023-04-11 00:42:55 -0400

    More updates to styling
    currently have the desktop version in a decent place

--- pages/_app.js
+++ pages/_app.js
@@ -1,39 +1,19 @@
-// import '@/styles/globals.css';
-// import Header from '@/components/Header';
-// import Footer from '@/components/Footer';
-// import { ThemeProvider } from '@mui/material/styles';
-// import CssBaseline from '@mui/material/CssBaseline';
-// import theme from '../theme';
-
-// function MyApp({ Component, pageProps }) {
-//   return (
-//     <ThemeProvider theme={theme}>
-//       <CssBaseline />
-//       <Header />
-//       <main>
-//         <Component {...pageProps} />
-//       </main>
-//       <Footer />
-//     </ThemeProvider>
-//   );
-// }
-
-// export default MyApp;
 import '@/styles/globals.css';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from '../theme';
-import styles from '@/styles/layout.module.css';
+import layoutStyles from '@/styles/layout.module.css';
+import contentStyles from '@/styles/content.module.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <div className={styles.container}>
+      <div className={layoutStyles.container}>
         <Header />
-        <main className={styles.content}>
+        <main className={`${layoutStyles.main} ${contentStyles.content}`}>
           <Component {...pageProps} />
         </main>
         <Footer />
