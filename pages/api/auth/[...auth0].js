e127286b48127fe146a8f0828f86a2025d5fbd57
kvin-stphns <kvin.stphns@gmail.com>
2023-04-14 19:53:39 -0400

    Trying to fix the callback issue

--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -3,7 +3,39 @@
 // export default handleAuth();
 
 
-import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
+// import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
+
+// const saveUserMetadata = async (user, context) => {
+//   const { firstName, lastName } = context.request.body;
+
+//   if (firstName && lastName) {
+//     const namespace = 'https://myapp.example.com/';
+//     user.app_metadata = user.app_metadata || {};
+//     user.app_metadata[namespace + 'first_name'] = firstName;
+//     user.app_metadata[namespace + 'last_name'] = lastName;
+//     await context.auth0.updateAppMetadata(user.user_id, user.app_metadata);
+//   }
+
+//   return user;
+// };
+
+// export default handleAuth({
+//   async login(req, res) {
+//     try {
+//       await handleLogin(req, res, {
+//         authorizationParams: {
+//           response_type: 'code id_token',
+//           scope: 'openid email profile',
+//         },
+//         afterCallback: saveUserMetadata,
+//       });
+//     } catch (error) {
+//       res.status(error.status || 400).end(error.message);
+//     }
+//   },
+// });
+
+import { handleAuth, handleCallback, handleLogin } from '@auth0/nextjs-auth0';
 
 const saveUserMetadata = async (user, context) => {
   const { firstName, lastName } = context.request.body;
@@ -19,7 +51,7 @@ const saveUserMetadata = async (user, context) => {
   return user;
 };
 
-export default handleAuth({
+const options = {
   async login(req, res) {
     try {
       await handleLogin(req, res, {
@@ -33,4 +65,23 @@ export default handleAuth({
       res.status(error.status || 400).end(error.message);
     }
   },
-});
+  async callback(req, res) {
+    try {
+      await handleCallback(req, res, {
+        afterCallback: async (req, res, session) => {
+          return {
+            ...session,
+            user: {
+              ...session.user,
+              id_token: req.oidc.id_token.__raw,
+            },
+          };
+        },
+      });
+    } catch (error) {
+      res.status(error.status || 500).end(error.message);
+    }
+  },
+};
+
+export default handleAuth(options);
